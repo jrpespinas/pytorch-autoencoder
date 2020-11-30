0d58fa7620273a6d39eaf99cdda74cc7f0559ed0
jrpespinas <jrpespinas@gmail.com>
2020-11-30 19:50:40 +0800

    feat: add encoder and decoder

--- model.py
+++ model.py
@@ -1,10 +1,31 @@
 import torch
-import torchvision 
+import torchvision
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.ReLU(True)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(16, 6, kernel_size=5),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(6, 3, kernel_size=5),
+            nn.ReLU(True),
+        )
+
+
 def main():
     pass
+
 
 if __name__ == "__main__":
     main()
